656b6572908abb818747de4f4f94e22012818a57
chikara <chenhongtu@51xianqu.net>
2017-04-22 19:30:22 +0800

    feat: build

--- manager/build/webpack.dev.config.js
+++ manager/build/webpack.dev.config.js
@@ -31,7 +31,7 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'babel',
             query: {
-                presets: ['es2015', 'react', 'stage-0', 'react-hmre'],
+                presets: ['es2015', 'react', 'stage-0'],
                 plugins: ['transform-runtime', 'add-module-exports']
             }
         }, {
